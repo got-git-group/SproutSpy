00e5d5a696acee0294daa0ee420401b7f5633f37
Samuli Jarvinen <frye@github.com>
2022-03-22 22:24:25 -0700

    Adding boilerplate to server.js and eslint with airbnb rules

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Temporary for PORT remove when the config is read in
+const PORT = 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+	console.log(`App started and listening on port ${PORT}`);
+})

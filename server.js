15ab8f049e14fe82450e8dee18d6eb2a67079a36
Samuli Jarvinen <frye@github.com>
2022-03-22 22:28:11 -0700

    Fix lint errors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 // Temporary for PORT remove when the config is read in
@@ -6,8 +7,8 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-	console.log(`App started and listening on port ${PORT}`);
-})
+  console.log(`App started and listening on port ${PORT}`);
+});

7e6aa96549a0e1a197975b93fb3548cb29a1f7d2
Andrew An <andyan7@gmail.com>
2022-03-26 11:08:45 -0700

    various updates to test

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const glossaryRoutes = require('./glossary-routes.js');
+const glossaryRoutes = require('./glossary-routes');
+const homeRoutes = require('./home-routes');
 
+router.use('/', homeRoutes);
 router.use('/glossary', glossaryRoutes);
 router.use('/api', apiRoutes);
 
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 sequelize.sync({ force: false })
   .then(() => {

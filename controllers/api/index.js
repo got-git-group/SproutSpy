f1fd232a29aa89f1409a5c46a2e3c4248dfa4c43
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-03-29 19:23:10 -0700

    created initial plant api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 // importing the various api routes
 const userRoutes = require('./user-routes');
 // const resultsRoutes = require('./results-routes');
-// const plantRoutes = require('./plant-routes');
+const plantRoutes = require('./plant-routes');
 
 router.use('/user', userRoutes);
 // router.use('/results', resultsRoutes);
-// router.use('/plants', plantRoutes);
+router.use('/plants', plantRoutes);
 
 module.exports = router;

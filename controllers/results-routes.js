67eb95e3189df18c48f8094b43b3ce6a11a3a9c7
Samuli Jarvinen <frye@github.com>
2022-03-31 19:18:38 -0700

    remove extra logging

--- controllers/results-routes.js
+++ controllers/results-routes.js
@@ -6,9 +6,8 @@ router.get('/', async (req, res) => {
     const plantData = await Plant.findAll({
       include: [Zone, Sunshine]
     });
-    
+
     const plants = plantData.map((plant) => plant.get({ plain: true }));
-    console.log(plants);
 
     res.render('results', {
       plants,
